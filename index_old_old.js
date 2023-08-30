69678bb3a0e7923d43f2d51d077c0fc5e84aad35
Shareef Ali <64172657+shareef945@users.noreply.github.com>
2023-08-30 20:06:03 +0000

    hubtel requestToPay complete

--- index_old_old.js
+++ index_old_old.js
@@ -103,56 +103,26 @@ app.post("/ussd/v2/rider", async (req, res) => {
     ClientState,
     Platform,
   } = req.body;
-  console.log("Hubtel req", req.body);
+  console.log(req.body);
   let response;
+
 
   const phoneRegex = /^0[2357][0-9]{8}$/;
 
-  const MerchantNum = Message.split("*")[3];
-  const msisdn = await convertToMsisdn(MerchantNum.slice(0, -1));
-  console.log(msisdn, "msisdn");
+  const MerchantNum = Message.split("*")[2];
 
   switch (Type) {
     case "Initiation":
-      // get the merchant
-      try {
-        const { merchant, merchantRoute } = await getMerchant(msisdn);
-        console.log(merchant, "merchant");
-        console.log(merchantRoute, "merchantRoute");
-        if (merchant.driver_phone_number == msisdn) {
-          // if exists, initiate the payment and add to cart
-          const item = {
-            ItemName: `Payment for ${merchantRoute.start_location} to ${merchantRoute.end_location} to ${merchant.driver_phone_number}`,
-            Qty: 1,
-            Price: Number(merchantRoute.fare),
-          };
-          // log the session id as a pending transaction
-
-          // then ask for approval
-          response = {
-            SessionId: SessionId,
-            Type: "AddToCart",
-            Item: item,
-            Message:
-              "Thank you for using this service. \nPlease approve the payment request on your phone to confirm the payment.",
-            Label: "Welcome to SAI Transport",
-            DataType: "display",
-            FieldType: "text",
-          };
-          break;
-        }
-      } catch (error) {
-        response = {
-          SessionId: SessionId,
-          Type: "release",
-          Message:
-            "Sorry, we couldn't find that merchant. Please check the number and try again.",
-          Label: "Welcome to SAI Transport",
-          DataType: "display",
-          FieldType: "text",
-        };
-        break;
-      }
+      response = {
+        SessionId: SessionId,
+        Type: "response",
+        Message:
+          "1. Transfer Money \n2. MomoPay & Pay Bill \n3. Airtime & Bundles \n4. Allow Cash Out \n5. Financial Services \n6. Wallet \n7. Transport",
+        Label: "Welcome to SAI Transport",
+        DataType: "input",
+        FieldType: "text",
+      };
+      break;
     case "Timeout":
       response = {
         SessionId: SessionId,
@@ -162,12 +132,425 @@ app.post("/ussd/v2/rider", async (req, res) => {
         DataType: "display",
         FieldType: "text",
       };
+    case "Response":
+      switch (Sequence) {
+        case 2:
+          //7
+          switch (String(Message)) {
+            case "7":
+              response = {
+                SessionId: SessionId,
+                Type: "response",
+                Message: "1. Merchant \n2. Rider",
+                Label: "Transport",
+                DataType: "input",
+                FieldType: "text",
+              };
+              break;
+          }
+        case 3:
+          //7*1 or 7*2
+          switch (String(Message)) {
+            case "1": // 7*1 merchant
+              // check if Merchant exists on DB
+              const result = await isMerchant(MerchantNum);
+              console.log("Is this a merchant? :", result);
+              try {
+                if (result.role == "driver") {
+                  response = {
+                    SessionId: SessionId,
+                    Type: "response",
+                    Message: "Enter your MTN Transport PIN to verify yourself:",
+                    Label: "Transport",
+                    DataType: "input",
+                    FieldType: "number",
+                    ClientState: "verify-merchant-pin",
+                  };
+                } else if (result.role == "mate") {
+                  response = {
+                    SessionId: SessionId,
+                    Type: "response",
+                    Message:
+                      "1. View transactions \n2. Initiate a rider payment",
+                    Label: "Transport",
+                    DataType: "input",
+                    FieldType: "number",
+                    ClientState: "mate",
+                  };
+                } else {
+                  response = {
+                    SessionId: SessionId,
+                    Type: "response",
+                    Message:
+                      "Welcome to MTN Transport. To start onboarding, enter your Driver License Number:",
+                    Label: "Transport",
+                    DataType: "input",
+                    FieldType: "text",
+                    ClientState: "onboard-merchant",
+                  };
+                }
+                break;
+              } catch (error) {
+                console.log(error);
+                response = {
+                  SessionId: SessionId,
+                  Type: "response",
+                  Message: "Error. Please try again later",
+                  Label: "Transport",
+                  DataType: "display",
+                  FieldType: "number",
+                };
+                break;
+              }
+            case "2": // 7*2
+              const success = await verifyRider(rider);
+              if (success) {
+                response = {
+                  SessionId: SessionId,
+                  Type: "response",
+                  Message: "1. Pay \n2. View Trips",
+                  Label: "Transport",
+                  DataType: "input",
+                  FieldType: "text",
+                  ClientState: "rider",
+                };
+              } else if (success == "stop") {
+                response = {
+                  SessionId: SessionId,
+                  Type: "response",
+                  Message:
+                    "Thank you for using our service, please accept the pre-approval request on your phone and start again",
+                  Label: "Transport",
+                  DataType: "input",
+                  FieldType: "text",
+                  ClientState: "rider",
+                };
+              } else {
+                response = {
+                  SessionId: SessionId,
+                  Type: "response",
+                  Message: "Rider verification error. Please try again later",
+                  Label: "Transport",
+                  DataType: "input",
+                  FieldType: "text",
+                  ClientState: "rider",
+                };
+              }
+              break;
+          }
+          break;
+        case 4:
+          // 7*1*1 7*2*1 7*2*2*
+          switch (ClientState) {
+            case "onboard-merchant":
+              break;
+            case "verify-merchant-pin":
+              try {
+                const { merchant: merchantObject, merchantRoute: route } =
+                  await getMerchant(MerchantNum);
+
+                const merchantPIN = merchantObject.pin;
+                // check if input is pin
+                const isPIN = await bcrypt.compare(Message, merchantPIN);
+                if (isPIN) {
+                  if (route?.start_location == "new user") {
+                    response = {
+                      SessionId: SessionId,
+                      Type: "response",
+                      Message:
+                        "To use MTN Transport as merchant, you should first create a route:\n1.New route",
+                      Label: "Transport",
+                      DataType: "input",
+                      FieldType: "number",
+                      ClientState: "merchant",
+                    };
+                    break;
+                  } else {
+                    response = {
+                      SessionId: SessionId,
+                      Type: "response",
+                      Message:
+                        "1. New route\n2. View route\n3. View transactons\n4. Manage account\n5. Initiate payment",
+                      Label: "Transport Merchant",
+                      DataType: "input",
+                      FieldType: "number",
+                      ClientState: "merchant",
+                    };
+                    break;
+                  }
+                } else {
+                  response = {
+                    SessionId: SessionId,
+                    Type: "response",
+                    Message: "Couldn't verify your PIN, please try again:",
+                    Label: "Transport",
+                    DataType: "input",
+                    FieldType: "number",
+                    ClientState: "verify-merchant-pin",
+                    Sequence: 3,
+                  };
+                  break;
+                }
+                break;
+              } catch {
+                response = {
+                  SessionId: SessionId,
+                  Type: "release",
+                  Message: "Ran into an error processing your request.",
+                  Label: "Transport",
+                  DataType: "input",
+                  FieldType: "number",
+                };
+                break;
+              }
+
+            case "mate":
+              // MERCHANT
+              switch (String(Message)) {
+                case "1": //7*1*1 Mate view transactions
+                  response = {
+                    SessionId: SessionId,
+                    Type: "response",
+                    Message: "Here is a new route",
+                    Label: "Transport",
+                    DataType: "input",
+                    FieldType: "number",
+                  };
+                  break;
+                case "2": //7*1*2 Mate initiate rider payment
+                  response = {
+                    SessionId: SessionId,
+                    Type: "response",
+                    Message:
+                      "Enter the number of the rider you want to charge:",
+                    Label: "Transport",
+                    DataType: "input",
+                    FieldType: "number",
+                  };
+                  break;
+              }
+              break;
+            case "rider":
+              // RIDER
+              switch (String(Message)) {
+                case "1": //7*2*1
+                  response = {
+                    SessionId: SessionId,
+                    Type: "response",
+                    Message: "Enter the merchant's number",
+                    Label: "Transport",
+                    DataType: "input",
+                    FieldType: "number",
+                    ClientState: "rider_pay",
+                  };
+                  break;
+                case "2": //7*2*2
+                  const tripList = await getTrips(Mobile);
+                  let recentTrips;
+                  let tripRes = "Here are your recent trips: ";
+                  if (tripList) {
+                    recentTrips = tripsList.join("\n\n");
+                    tripRes = tripRes + recentTrips;
+                    response = {
+                      SessionId: SessionId,
+                      Type: "response",
+                      Message: tripRes,
+                      Label: "Transport",
+                      DataType: "input",
+                      FieldType: "number",
+                    };
+                  } else {
+                    tripRes = "You have no trips yet";
+                    response = {
+                      SessionId: SessionId,
+                      Type: "release",
+                      Message: tripRes,
+                      Label: "Transport",
+                      DataType: "input",
+                      FieldType: "display",
+                      Data: null,
+                    };
+                  }
+                  break;
+              }
+              break;
+          }
+          break;
+        case 5:
+          // 7*1*1*1 7*2*1*[merchant number]
+          switch (ClientState) {
+            case "merchant":
+              switch (String(Message)) {
+                case "1":
+                  response = {
+                    SessionId: SessionId,
+                    Type: "response",
+                    Message: `Enter the name of the Start Location:`,
+                    Label: "Transport Merchant | Create a route",
+                    DataType: "display",
+                    FieldType: "text",
+                    ClientState: "new-route",
+                  };
+                  break;
+                case "2":
+                  try {
+                    const { merchant: merchantObject, merchantRoute: route } =
+                      await getMerchant(MerchantNum);
+                    response = {
+                      SessionId: SessionId,
+                      Type: "response",
+                      Message: `Start Location: ${route?.start_location}\nEnd Location: ${route?.end_location}\nFare: ${route.fare}\n1. Change Start Location\n2. Change End Location\n3. Change Fare`,
+                      Label: "Transport Merchant | View route",
+                      DataType: "display",
+                      FieldType: "text",
+                      ClientState: "view-route",
+                    };
+                    break;
+                  } catch {
+                    response = {
+                      SessionId: SessionId,
+                      Type: "release",
+                      Message: "Error. Please start over.",
+                      Label: "Transport",
+                      DataType: "input",
+                      FieldType: "text",
+                    };
+                  }
+                  break;
+                case "3":
+                  try {
+                    // recent merchant transactions
+                    const transactions = await getTransactionsForMerchant(
+                      MerchantNum
+                    );
+                    const recentTransactions = transactions
+                      ?.reverse()
+                      ?.slice(0, 10); // reverse the order of the transactions and get the last 10
+                    let transactionString =
+                      "Here are your recent transactions:\n";
+                    recentTransactions?.forEach((transaction) => {
+                      const amount = transaction?.amount;
+                      const sender = transaction?.sender_account;
+                      const timestamp = new Date(
+                        transaction?.timestamp_created
+                      );
+
+                      transactionString += `${amount} GHS from ${sender} on ${timestamp.toLocaleString()}\n`;
+                    });
+                    if (recentTransactions.length === 0) {
+                      transactionString = "There are no recent transactions.";
+                    }
+                    response = {
+                      SessionId: SessionId,
+                      Type: "release",
+                      Message: transactionString,
+                      Label: "Transport Merchant | View Route",
+                      DataType: "display",
+                      FieldType: "text",
+                    };
+                    break;
+                  } catch {
+                    response = {
+                      SessionId: SessionId,
+                      Type: "release",
+                      Message: "Error. Please start over.",
+                      Label: "Transport",
+                      DataType: "input",
+                      FieldType: "text",
+                    };
+                  }
+                  break;
+              }
+              break;
+            case "rider_pay": // 7*2*1*[merchant number]
+              if (phoneRegex.test(Message)) {
+                const merchantNumber = convertToMsisdn(Message);
+                try {
+                  const { merchant, merchantRoute } = await getMerchant(
+                    merchantNumber
+                  );
+                  if (merchant.phone_number === merchantNumber) {
+                    const success = await requestToPay(
+                      Mobile,
+                      merchantNumber,
+                      "rider-side"
+                    );
+                    if (success) {
+                      response = {
+                        SessionId: SessionId,
+                        Type: "response",
+                        Message: `Thank you for paying. Your trip has been booked \nStart Location:${merchantRoute.start_location} \nEnd Location:${merchantRoute.end_location} \nFare:${merchantRoute.fare}`,
+                        Label: "Transport",
+                        DataType: "display",
+                        FieldType: "text",
+                      };
+                      try {
+                        const newTrip = await createTrip(rider, merchantNumber);
+                        console.log(newTrip);
+                      } catch (error) {
+                        console.log(error);
+                      }
+                    } else {
+                      response = {
+                        SessionId: SessionId,
+                        Type: "response",
+                        Message: "Payment failed. Please try again later",
+                        Label: "Transport",
+                        DataType: "input",
+                        FieldType: "number",
+                      };
+                    }
+                  } else {
+                    response = {
+                      SessionId: SessionId,
+                      Type: "response",
+                      Message: "Merchant not found. Please try again later",
+                      Label: "Transport",
+                      DataType: "display",
+                      FieldType: "number",
+                    };
+                  }
+                } catch (error) {
+                  console.log(error);
+                  response = {
+                    SessionId: SessionId,
+                    Type: "response",
+                    Message: "Error. Please try again later",
+                    Label: "Transport",
+                    DataType: "display",
+                    FieldType: "number",
+                  };
+                }
+              } else {
+                response = {
+                  SessionId: SessionId,
+                  Type: "release",
+                  Message: "Invalid input",
+                  Label: "Transport",
+                  DataType: "input",
+                  FieldType: "text",
+                };
+              }
+          }
+          break;
+        default:
+          response = {
+            SessionId: SessionId,
+            Type: "release",
+            Message: "Invalid input",
+            Label: "Transport",
+            DataType: "input",
+            FieldType: "text",
+          };
+          break;
+      }
       break;
   }
   res.set("Content-Type", "application/json");
   res.send(response);
-  // console.log(response);
+  console.log(response);
 });
+
 
 app.post("/ussd/v2/africastalking", async (req, res) => {
   const { text, phoneNumber } = req.body;
